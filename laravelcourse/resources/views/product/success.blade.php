11b36902f7cfd5893eb44f7110204898944e7b03
stufonn-cell <stufonn@gmail.com>
2025-08-10 15:09:17 -0500

    Add: Success view

---
+++ laravelcourse/resources/views/product/success.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('title', "Product Created")
+@section('subtitle', "SUCCESS")
+@section('content')
+<div class="alert alert-success text-center">
+    Product created successfully!
+</div>
+@endsection
