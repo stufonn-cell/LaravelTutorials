7af2cf91edc08f14ed215b0da446de8b1b8e7464
stufonn-cell <stufonn@gmail.com>
2025-08-10 14:43:22 -0500

    Fix: Removing whitespaces

--- laravelcourse/routes/web.php
+++ laravelcourse/routes/web.php
@@ -2,31 +2,31 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'App\Http\Controllers\HomeController@index')->name("home.index");
+Route::get('/', 'App\Http\Controllers\HomeController@index')
+    ->name("home.index");
 
 Route::get('/about', function () {
-
     $data1 = "About us - Online Store";
-
     $data2 = "About us";
-
     $description = "This is an about page ...";
-
     $author = "Developed by: Miguel Ángel Cano Salinas";
 
     return view('home.about')->with("title", $data1)
-
         ->with("subtitle", $data2)
-
         ->with("description", $description)
-
         ->with("author", $author);
-
 })->name("home.about");
 
 Route::get('/contact', function () {
     $email = "salinitas@rappi.com";
     $address = "Calle 44B Norte #84-44";
     $phone = "+57 3244654565";
+
     return view('home.contact', compact('email', 'address', 'phone'));
 })->name("home.contact");
+
+Route::get('/products', 'App\Http\Controllers\ProductController@index')
+    ->name("product.index");
+
+Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')
+    ->name("product.show");
